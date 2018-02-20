58dfd239d41f6dc23894afbce8ecb8eb18ecbda3
github username <github email address>
2018-02-20 17:23:32 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash = Hash.new(0)
+
+  #as you iterate through the array, you need to turn the array string elements into string hash keys.
+  #Check to see if a key is already present and if it is, increment it by 1.
+
+    array.each do |each_element|
+      new_hash[each_element] += 1
+
+    end
+new_hash
 end
- 
